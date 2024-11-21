4963a801205c9f97ae470180cf6b1252676c5a77
Sadiya <shaiksadiya117@gmail.com>
2024-11-21 20:53:58 +0530

    updates terraform

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "myserver" {
+  ami = "ami-012967cc5a8c9f891"
+  instance_type = "t2.micro"
+  tags = {
+    name = "sampleserver"
+  }
+}
