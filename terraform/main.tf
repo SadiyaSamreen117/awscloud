36f8298f63115fd881a0d4713da4b0b8d8a79276
Sadiya <shaiksadiya117@gmail.com>
2024-11-22 16:53:59 +0530

    updates main

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,17 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.77.0"
+    }
+  }
 }
 
-resource "aws_instance" "myserver" {
-  ami = "ami-012967cc5a8c9f891"
-  instance_type = "t2.micro"
-  tags = {
-    name = "sampleserver"
-  }
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-012967cc5a8c9f891"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
 }
