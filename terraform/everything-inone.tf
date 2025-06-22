0e87afdffff3ebbb181873ba8ff887b695e6d312
sadiya <shaiksadiya117@gmail.com>
2025-06-22 23:36:02 +0530

    whatever u want to write as commit meaasage. U will write any thing feel free to write commit msg.  U will also include walger words dont worry. :)

---
+++ terraform/everything-inone.tf
@@ -0,0 +1,87 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+#vpc
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main_vpc"
+  }
+}
+
+#subnet
+resource "aws_subnet" "main_subnet" {
+  vpc_id = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "main_subnet"
+  }
+}
+
+#internet gateway
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = {
+    Name = "main_igw"
+  }
+}
+
+#route table
+resource "aws_route_table" "main_route_table" {
+  vpc_id = aws_vpc.main_vpc.id
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+  tags = {
+    Name = "main_route_table"
+  }
+}
+
+#route table assosiation
+resource "aws_route_table_association" "main_assoc" {
+  subnet_id = aws_subnet.main_subnet.id
+  route_table_id = aws_route_table.main_route_table.id
+}
+
+#security group
+resource "aws_security_group" "main_sg" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main_vpc.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-ssh"
+  }
+}
+
+#ec2 instance
+resource "aws_instance" "main_ec2" {
+  ami = "ami-9876543210abbc"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.main_subnet.id
+  vpc_security_group_ids = [aws_security_group.main_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "main_ec2"
+  }
+}
